4d886d991a07cb41f0bb25222d434c1d47182671
micsta00 <110664760+micsta00@users.noreply.github.com>
2023-10-17 12:23:10 +0200

    Create header without functionality

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,18 +1,45 @@
-import { Menu } from "lucide-react"
+import { Menu, Upload, Bell, User, Mic, Search } from "lucide-react"
 import logo from "../assets/logo.png"
 import { Button } from "../components/Button"
 
 export function PageHeader() {
-    return (<div className="flex gap-10 lg:gap=20 justify-between">
+    return (<div className="flex gap-10 lg:gap=20 justify-between pt-2 mb-6 mx-4">
         <div className="flex gap-4 items-center flex-shrink-0">
-            <Button variant="ghost" size="icon" className="bg-red-500">
+            <Button variant="ghost" size="icon">
                 <Menu />
             </Button>
             <a href="/">
                 <img src={logo} alt="" className="h-6" />
             </a>
         </div>
-        <div></div>
-        <div></div>
+        <form className="md:flex hidden gap-4 flex-grow justify-center">
+            <div className="flex flex-grow max-w-[600px]">
+                <input type="search" placeholder="Search" className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none" />
+                <Button className="py-2 px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0">
+                    <Search />
+                </Button>
+            </div>
+            <Button type="button" size="icon" className="flex-shrink-0">
+                <Mic />
+            </Button>
+
+        </form>
+        <div className="flex flex-shrink-0 md:gap-2">
+            <Button size="icon" variant="ghost" className="md:hidden">
+                <Search />
+            </Button>
+            <Button size="icon" variant="ghost" className="md:hidden">
+                <Mic />
+            </Button>
+            <Button size="icon" variant="ghost">
+                <Upload />
+            </Button>
+            <Button size="icon" variant="ghost">
+                <Bell />
+            </Button>
+            <Button size="icon" variant="ghost">
+                <User />
+            </Button>
+        </div>
     </div>)
 }
