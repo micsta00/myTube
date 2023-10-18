b5ffee74120b18c4051e15c6946318ca8fb971a8
micsta00 <110664760+micsta00@users.noreply.github.com>
2023-10-18 21:25:54 +0200

    Create menu - subscription section

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -37,41 +37,52 @@ export function Sidebar() {
             </aside>
             <aside className="w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 flex">
                 <LargeSidebarSection title="Hi" >
-                    <LargeSidebarItem isActive Icon={Home} title="Home" url="/" />
-                    <LargeSidebarItem Icon={Clapperboard} title="Subscriptions" url="/subscriptions" />
+                    <LargeSidebarItem isActive IconOrImgUrl={Home} title="Home" url="/" />
+                    <LargeSidebarItem IconOrImgUrl={Clapperboard} title="Subscriptions" url="/subscriptions" />
 
                 </LargeSidebarSection>
                 <hr />
                 <LargeSidebarSection visibleItemCount={5}>
                     <LargeSidebarItem
-                        Icon={Library}
+                        IconOrImgUrl={Library}
                         title="Library"
                         url="/library"
                     />
                     <LargeSidebarItem
-                        Icon={History}
+                        IconOrImgUrl={History}
                         title="History"
                         url="/history"
                     />
                     <LargeSidebarItem
-                        Icon={PlaySquare}
+                        IconOrImgUrl={PlaySquare}
                         title="Your Videos"
                         url="/your-videos"
                     />
                     <LargeSidebarItem
-                        Icon={Clock}
+                        IconOrImgUrl={Clock}
                         title="Watch Later"
                         url="/playlist?list=WL"
                     />
                     {playlists.map(playlist => (
                         <LargeSidebarItem
                             key={playlist.id}
-                            Icon={ListVideo}
+                            IconOrImgUrl={ListVideo}
                             title={playlist.name}
                             url={`/playlist?list=${playlist.id}`}
                         />
                     ))}
 
+                </LargeSidebarSection>
+                <hr />
+                <LargeSidebarSection title="Subscriptions">
+                    {subscriptions.map(subscription => (
+                        <LargeSidebarItem
+                            key={subscription.id}
+                            IconOrImgUrl={subscription.imgUrl}
+                            title={subscription.channelName}
+                            url={`/@${subscription.id}`}
+                        />
+                    ))}
                 </LargeSidebarSection>
                 <hr />
 
@@ -117,18 +128,23 @@ function LargeSidebarSection({ children, title, visibleItemCount = Number.POSITI
 }
 
 type LargeSidebarItemProps = {
-    Icon: ElementType
+    IconOrImgUrl: ElementType | string
     title: string
     url: string
     isActive?: boolean
 }
 
 
-function LargeSidebarItem({ Icon, title, url, isActive = false }: LargeSidebarItemProps) {
+function LargeSidebarItem({ IconOrImgUrl, title, url, isActive = false }: LargeSidebarItemProps) {
     return <a href={url} className={twMerge(buttonStyles({ variant: "ghost" }), `w-full flex items-center rounded-lg gap-4 p-3 ${isActive ? "font-bold bg-neutral-100 hover:bg-secondary" : undefined}`)}>
-        <Icon className="w-6 h-6" />
+        {typeof IconOrImgUrl === "string" ? (
+            <img src={IconOrImgUrl} className="w-6 h-6 rounded-full" />
+        ) : (
+            <IconOrImgUrl className="w-6 h-6" />
+        )}
         <div className="whitespace-nowrap overflow-hidden text-ellipsis">
             {title}
         </div>
+
     </a>
 }
