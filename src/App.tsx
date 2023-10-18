b1d85eb5e281edf1b1ef31a8b1d46eaad8c5f3d9
micsta00 <110664760+micsta00@users.noreply.github.com>
2023-10-18 21:06:41 +0200

    Create menu - first two section

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { PageHeader } from "./layouts/PageHeader"
 import { categories, videos } from "./data/home"
 import { useState } from "react"
 import { VideoGridItem } from "./components/VideoGridItem"
+import { Sidebar } from "./layouts/Sidebar"
 
 export default function App() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0])
   return (<div className="max-h-screen flex flex-col">
     <PageHeader />
     <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
-      <div>Sidebar</div>
+      <Sidebar />
       <div className="overflow-x-hidden px-8 pb-4">
         <div className="sticky top-0 bg-white z-10 pb-4">
           <CategoryPills categories={categories} selectedCategory={selectedCategory} onSelect={setSelectedCategory} />
