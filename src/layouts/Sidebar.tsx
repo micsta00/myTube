3fa70107b0e90f139329231b34bb17c92faf1ab7
micsta00 <110664760+micsta00@users.noreply.github.com>
2023-10-18 22:13:48 +0200

    Add scrollbar styling

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -35,8 +35,8 @@ export function Sidebar() {
                 <SmallSidebarItem Icon={Clapperboard} title="Subscriptions" url="/subscriptions" />
                 <SmallSidebarItem Icon={Library} title="Library" url="/library" />
             </aside>
-            <aside className="w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 flex">
-                <LargeSidebarSection title="Hi" >
+            <aside className="w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 lg:flex hidden">
+                <LargeSidebarSection>
                     <LargeSidebarItem isActive IconOrImgUrl={Home} title="Home" url="/" />
                     <LargeSidebarItem IconOrImgUrl={Clapperboard} title="Subscriptions" url="/subscriptions" />
 
@@ -85,6 +85,51 @@ export function Sidebar() {
                     ))}
                 </LargeSidebarSection>
                 <hr />
+                <LargeSidebarSection title="Explore">
+                    <LargeSidebarItem
+                        IconOrImgUrl={Flame}
+                        title="Trending"
+                        url="/trending"
+                    />
+                    <LargeSidebarItem
+                        IconOrImgUrl={ShoppingBag}
+                        title="Shopping"
+                        url="/shopping"
+                    />
+                    <LargeSidebarItem IconOrImgUrl={Music2} title="Music" url="/music" />
+                    <LargeSidebarItem
+                        IconOrImgUrl={Film}
+                        title="Movies & TV"
+                        url="/movies-tv"
+                    />
+                    <LargeSidebarItem IconOrImgUrl={Radio} title="Live" url="/live" />
+                    <LargeSidebarItem
+                        IconOrImgUrl={Gamepad2}
+                        title="Gaming"
+                        url="/gaming"
+                    />
+                    <LargeSidebarItem IconOrImgUrl={Newspaper} title="News" url="/news" />
+                    <LargeSidebarItem
+                        IconOrImgUrl={Trophy}
+                        title="Sports"
+                        url="/sports"
+                    />
+                    <LargeSidebarItem
+                        IconOrImgUrl={Lightbulb}
+                        title="Learning"
+                        url="/learning"
+                    />
+                    <LargeSidebarItem
+                        IconOrImgUrl={Shirt}
+                        title="Fashion & Beauty"
+                        url="/fashion-beauty"
+                    />
+                    <LargeSidebarItem
+                        IconOrImgUrl={Podcast}
+                        title="Podcasts"
+                        url="/podcasts"
+                    />
+                </LargeSidebarSection>
 
             </aside>
         </>)
