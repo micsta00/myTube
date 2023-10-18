d2a02721a8a977c3a92161ecaaa9ecdd6748a41c
micsta00 <110664760+micsta00@users.noreply.github.com>
2023-10-18 10:54:12 +0200

    Videos playing set
    after hover

--- src/components/VideoGridItem.tsx
+++ src/components/VideoGridItem.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { formatDuration } from "../utils/formatDuration"
 import { formatTimeAgo } from "../utils/formatTimeAgo"
 
@@ -20,10 +20,24 @@ type VideoGripItemProps = {
 const VIEW_FORMATTER = new Intl.NumberFormat(undefined, { notation: "compact" })
 
 export function VideoGridItem({ id, title, channel, views, postedAt, duration, thumbnailUrl, videoUrl }: VideoGripItemProps) {
-    return <div className="flex flex-col gap-2">
+    const [isVideoPlaying, setIsVideoPlaying] = useState(false)
+    const videoRef = useRef<HTMLVideoElement>(null)
+
+    useEffect(() => {
+        if (videoRef.current == null) return
+
+        if (isVideoPlaying) {
+            videoRef.current.currentTime = 0
+            videoRef.current.play()
+        } else {
+            videoRef.current.pause()
+        }
+    }, [isVideoPlaying])
+    return <div className="flex flex-col gap-2" onMouseEnter={() => setIsVideoPlaying(true)} onMouseLeave={() => setIsVideoPlaying(false)}>
         <a href={`/watch?v=${id}`} className="relative aspect-video">
-            <img src={thumbnailUrl} className="block w-full h-full object-cover rounded-xl" />
+            <img src={thumbnailUrl} className={`block w-full h-full object-cover transition-[border-radius] duration-200 ${isVideoPlaying ? "rounded-none" : "rounded-xl"}`} />
             <div className="absolute bottom-1 right-1 bg-secondary-dark text-secondary text-sm px-0.5 rounded">{formatDuration(duration)}</div>
+            <video className={`block h-full object-cover absolute inset-0 transition-opacity duration-200  ${isVideoPlaying ? "opacity-100 delay-200" : "opacity-0"}`} ref={videoRef} muted playsInline src={videoUrl} />
         </a>
         <div className="flex gap-2">
             <a href={`/@${channel.id}`} className="flex-shrink-0">
@@ -39,6 +53,7 @@ export function VideoGridItem({ id, title, channel, views, postedAt, duration, t
                 <div className="text-secondary-text text-sm">
                     {VIEW_FORMATTER.format(views)} Views • {formatTimeAgo(postedAt)}
                 </div>
+
             </div>
 
         </div>
